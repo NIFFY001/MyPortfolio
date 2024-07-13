381cf0c2ec8b7407b2698ebc83aa651c03258e4e
Akinsanmi Victor <victorakinsanmi2018@gmail.com>
2024-07-13 19:53:59 +0100

    newpush

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,51 +15,51 @@ function NavBar() {
       className="flex justify-between items-center px-5 md:px-10 md:py-2 py-6  text-[#D9D9D9] bg-[#080808] sticky top-0 z-30"
       style={{ borderBottom: "1px solid #FFFFFF" }}
     >
-      <a href="/">
+      <Link to="/">
         <img src={logo} alt="Logo" className="h-[40px] md:h-[50px]" />
-      </a>
+      </Link>
 
       <div className="hidden md:flex items-center gap-4 md:gap-10">
-        <a href="">
+        <Link to href="">
           <button className="hover:border-b hover:pb-1 text-xs md:text-sm">
             About
           </button>
-        </a>
-        <a href="">
+        </Link>
+        <Link to="">
           <button className="hover:border-b hover:pb-1 text-xs md:text-sm">
             Projects
           </button>
-        </a>
+        </Link>
         <Link to="/cont">
           <button className="hover:border-b hover:pb-1 text-xs md:text-sm">
             Contact
           </button>
         </Link>
       </div>
-      <a href="" target="blank" className="hidden md:block">
+      <Link to="" target="blank" className="hidden md:block">
         <button className=" font-semibold py-2 px-2 md:px-5 hover:border hover:border-white rounded-2xl text-xs md:text-sm bg-gradient-to-r from-[#FFA500] to-[#FF0000]">
           Download Resume
         </button>
-      </a>
+      </Link>
       <button className="md:hidden" onClick={toggleMobileMenu}>
         <FaBars size={25} color="#D9D9D9" />
       </button>
       {isMobileMenuOpen && (
         <div className="absolute top-full left-0 w-full bg-[#080808] text-[#D9D9D9] flex flex-col items-center gap-4 py-5">
-          <a href="" className="hover:border-b hover:pb-1 text-sm">
+          <Link to="" className="hover:border-b hover:pb-1 text-sm">
             About
-          </a>
-          <a href="" className="hover:border-b hover:pb-1 text-sm">
+          </Link>
+          <Link to="" className="hover:border-b hover:pb-1 text-sm">
             Projects
-          </a>
+          </Link>
           <Link to="/cont" className="hover:border-b hover:pb-1 text-sm">
             Contact
           </Link>
-          <a href="" target="blank">
+          <Link to="" target="blank">
             <button className="hover:bg-[#fff] text-[#D9D9D9] font-semibold py-2 px-5 hover:border hover:border-white rounded-2xl text-sm bg-gradient-to-r from-[#FFA500] to-[#FF0000]">
               Download Resume
             </button>
-          </a>
+          </Link>
         </div>
       )}
     </nav>
